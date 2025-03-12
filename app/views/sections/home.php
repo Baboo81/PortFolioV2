44e46841bedf29056df2ee0fc3847491628a62ef
Baboo81 <magicbaboo@gmail.com>
2025-03-12 10:59:20 +0100

    Modif

--- app/views/sections/home.php
+++ app/views/sections/home.php
@@ -1,10 +1,14 @@
 <?php include __DIR__ . '/../partials/header.php'; ?>
 
 <section>
-    <div class="video-container">
-        <video src="">
-            
+    <div class="hero">
+        <video autoplay loop muted playsinline class="bg-video">
+            <source src="../public/videos/background.mp4" type="video/mp4">
+            Votre navigateur ne supporte pas les vidéos !  
         </video>
+        <div class="hero-content d-flex align-items-center justify-content-center">
+            <h1 class="homeTitle">WEB DEVELOPER</h1>
+        </div>
     </div>
 </section>
 
